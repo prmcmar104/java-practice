bdd8a2f0c479e3420dca0a82839e8303bf1a7c43
prmcsep103 <primecommunity103@gmail.com>
2024-04-27 09:58:10 +0100

    Morning Challenge

--- src/morningchallenge/Challenge1.java
+++ src/morningchallenge/Challenge1.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 /**
  * Created by Jay Vaghani
  */
+
+/**
+ * Write a java program using the following requirement.
+ * 1.Create a Java class with name 'Challenge1'
+ * 2.Create a main method.
+ * 3.Declare the Two String type variables with names 'name' and 'message' and assign value is
+ *   'name'= 'Your name(Jay)' and 'message'= 'Good Morning'
+ * 4.Print the following message using above variables, println statements and Concatenation.
+ */
 public class Challenge1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
